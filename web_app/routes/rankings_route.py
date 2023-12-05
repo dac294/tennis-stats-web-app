d3ec19f58b550db5ec96c9036296bedce07d911b
co1etti <dac294@georgetown.edu>
2023-12-05 18:46:55 -0500

    Update rankings_route.py

---
+++ web_app/routes/rankings_route.py
@@ -0,0 +1,31 @@
+
+# this is the "web_app/routes/rankings_routes.py" file...
+
+from flask import Blueprint, request, render_template, redirect, flash
+
+from app.rankings import get_game_info
+
+rankings_routes = Blueprint("rankings_routes", __name__)
+
+@rankings_routes.route("/rankings/dashboard")
+
+def rankings_dashboard():
+
+    try:
+        game_info = get_game_info()
+        #HERE I NEED TO FIGURE OUT HOW TO GET WHAT I WANT!!!!
+
+
+
+        flash("Latest Rankings Data!", "success")
+        return render_template("rankings.html", game_info=game_info)
+    
+
+    except Exception as err:
+        print('OOPS', err)
+
+        flash("Error. Please try again!", "danger")
+        return redirect("/")
+    
+
+#test
