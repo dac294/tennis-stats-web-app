8e687ca40b2c8944e99fc6989e877f02f263dfba
co1etti <dac294@georgetown.edu>
2023-12-05 18:46:08 -0500

    Update rankings.py

--- app/rankings.py
+++ app/rankings.py
@@ -1,35 +1,32 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template
+import http.client
+import json
+import datetime as dt
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
+def get_game_info(date):
+    conn = http.client.HTTPSConnection("api.sportradar.us")
+    conn.request("GET", f"https://api.sportradar.com/tennis/trial/v2/en/players/rankings.json?api_key={API_KEY}")
+
+    res = conn.getresponse()
+    data = res.read().decode("utf-8")
+    json_resp = json.loads(data)
+    return json_resp
+'''
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-@app.route('/get_tennis_stats', methods=['POST'])
-def get_tennis_stats():
-    player_name = request.form['player_name']
-
-    # Will input the API key in production
-    api_key = 'YOUR_API_KEY'
-    api_endpoint = 'TENNIS_API_ENDPOINT'
-
-    # Make a request to the Tennis API
-    response = requests.get(api_endpoint, params={'player_name': player_name, 'api_key': api_key})
-
-    if response.status_code == 200:
-        tennis_stats = response.json()
-        return render_template('tennis_stats.html', player_name=player_name, stats=tennis_stats)
-    else:
-        return render_template('error.html', message=f"Error fetching Tennis stats for {player_name}")
+    date = dt.date.today()
+    game_info = get_game_info(date)
+    return render_template('index.html', game_info=game_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
-'''
-    live_matches, upcoming_matches = get_tennis_matches(api_key, api_endpoint)
-
-    if live_matches is not None and upcoming_matches is not None:
-        display_matches_summary(live_matches, upcoming_matches)
 
 '''
+
