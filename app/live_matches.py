ef145857c7d50701d921a5f0711c76a273adca37
dac294 <dac294@georgetown.edu>
2023-12-14 15:20:19 -0500

    updating live match stats

--- app/live_matches.py
+++ app/live_matches.py
@@ -1,5 +1,5 @@
-from rankings import rankings
-from rankings import get_game_info
+import http.client
+import json
 import datetime as dt
 import os
 from dotenv import load_dotenv
@@ -7,22 +7,23 @@ import pandas as pd
 
 load_dotenv()
 
+
 API_KEY = os.getenv("API_KEY")
 date = dt.date.today()
-API_KEY = "3z3vm5s67q4hdabtdmcsxsnd"
-
-comp_ID = rankings()
-print(comp_ID)
-
-player_link = f"https://api.sportradar.com/tennis/trial/v3/en/schedules/live/timelines.json?api_key={API_KEY}"
-
-player_results = get_game_info(date,player_link)
-print(player_link)
-#print(player_results)
 
 
-df = pd.json_normalize(player_results)
-print(df)
+conn = http.client.HTTPSConnection("api.sportradar.com")
+live_link = f"https://api.sportradar.com/tennis/trial/v3/en/schedules/live/timelines.json?api_key={API_KEY}"
+
+
+conn.request("GET", live_link)
+
+res = conn.getresponse()
+data = res.read()
+json_resp = json.loads(data) 
+print(data.decode("utf-8"))
+
+
 
 
 
